68e80374c403cfb9cc2e494402a33d0ea237251b
seldenleonard <selden222@gmail.com>
2021-01-29 16:50:54 -0500

    cleans up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
-
-
 
   # outside of api namespace
   get "/products" => "products#index"
