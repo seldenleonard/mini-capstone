f4e936959b83150acb6d72b9622d1f342fdc3a4f
seldenleonard <selden222@gmail.com>
2021-01-29 16:41:45 -0500

    deletes previous carted products create action attempts

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -7,40 +7,6 @@ class Api::OrdersController < ApplicationController
     render "index.json.jb"
   end
 
-  # def create
-  #   @order = Order.new(
-  #     # order_id: order.id,
-  #     user_id: current_user.id,
-  #     product_id: params[:product_id], 
-  #     quantity: params[:quantity]
-  #     )
-  #     if @order.save
-  #       @order.subtotal = @order.quantity * Product.find(@order.product_id).price 
-  #       @order.tax = @order.subtotal * 0.09
-  #       @order.total = @order.subtotal + @order.tax
-  #       render "show.json.jb"
-  #     else
-  #       render json: {message: "failed"}, status: 422
-  #     end
-  #   end
-
-  # def create
-  #   @order = Order.new(
-  #     user_id: current_user.id,
-  #     product_id: params[:product_id], 
-  #     quantity: params[:quantity]
-  #     )
-  #     @order.subtotal = @order.quantity * @order.product.price
-  #     # OR    @order.subtotal = params[:quantity].to_i * @order.product.price
-  #     @order.tax = @order.subtotal * 0.09
-  #     @order.total = @order.tax + @order.subtotal
-  #     if @order.save
-  #       render "show.json.jb"
-  #     else
-  #       render json: {message: "failed"}, status: 422
-  #     end
-  # end
-
   def create
     carted_products = current_user.carted_products.where(status: "carted")
     
