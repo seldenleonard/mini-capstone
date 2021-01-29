23c76b2d65b0ff4dab490352f04ae3dbd0186246
seldenleonard <selden222@gmail.com>
2021-01-29 16:47:42 -0500

    adds explanation of under the hood work that rails is doing in supplier.rb

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -2,7 +2,9 @@ class Supplier < ApplicationRecord
   
   has_many :products
   
+  # The below method means the same as the above association. Rails is just doing the shorthand with "has_many :products"
   # def products
   #   Product.where(supplier_id: id)
   # end
+
 end
