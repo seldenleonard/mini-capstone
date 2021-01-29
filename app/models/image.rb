7fc5095d5b25bb2e062cf9897a02f0261d6a2c5d
seldenleonard <selden222@gmail.com>
2021-01-29 16:46:03 -0500

    returns what I previosuly deleted from image.rb and adds explanation of under-the-hood work that rails is doing to associate images and products

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,12 @@
 class Image < ApplicationRecord
   
+  
   validates :url, presence: true
   belongs_to :product
   
+  # The below method means the same as the above association. Rails is just doing the shorthand with "belongs_to :product"
+  # def products
+  #   Product.find_by(id: product_id)
+  # end
+
 end
